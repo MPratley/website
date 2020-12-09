a33c0db6e913cf16aff7d395d17449a890ebd4b8
Marcus Pratley <marcus.pratley@bambooloans.com>
2020-12-09 18:05:22 +0000

    better images

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 const package = require('../../package.json')
 
 const urlRoots = {
-  development: 'http://localhost:8081',
+  development: 'http://localhost:8080',
   production: 'https://www.mjp.dev',
 }
 
@@ -12,6 +12,8 @@ module.exports = {
   type: 'website',
   url: urlRoots[process.env.NODE_ENV],
   github: package.repository.url,
-  image: 'https://cdn.shopify.com/s/files/1/0176/3274/products/Pi-Zero-W-1_1000x.jpg',
+  image:
+    urlRoots[process.env.NODE_ENV] +
+    '/assets/static/img/hello_from_marcus_preview.webp',
   description: package.description,
 }
